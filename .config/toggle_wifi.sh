1949678d4965a1b146b387c2030e8e5fab54b32f
Victor <TKVictor-Hang@outlook.fr>
2022-10-31 22:28:32 +0100

    fixes

--- .config/toggle_wifi.sh
+++ .config/toggle_wifi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ $(iwctl device wlan0 show | grep "Powered             on" | wc -c) -eq 0 ]
+if [ $(iwctl device wlan0 show | grep "Powered               on" | wc -c) -eq 0 ]
 then
   iwctl device wlan0 set-property Powered on
 else
